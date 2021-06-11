aab7d10678a0e8efbe109f6213152b2623b19189
disha1822 <disha108a@gmail.com>
2021-06-11 17:54:15 +0530

    Angular Components and Routing implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
+import { CustomerComponent } from './customer/customer.component';
+import { TestComponent } from './test/test.component';
+import { LabComponent } from './lab/lab.component';
+import { AdminComponent } from './admin/admin.component';
+import { HomeComponent } from './home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent},
+  { path: 'admin', component: AdminComponent},
+  { path: 'lab', component: LabComponent},
+  { path: 'test', component: TestComponent},
+  { path: 'customer', component: CustomerComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { LabComponent } from './lab/lab.component';
+import { CustomerComponent } from './customer/customer.component';
+import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    LabComponent,
+    CustomerComponent,
+    TestComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
